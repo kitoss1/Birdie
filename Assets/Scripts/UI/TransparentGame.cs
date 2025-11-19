fabe3e111a4374c89908e54cf0c82930a4679a3b
MARCPC\marcg <marc.g.e@hotmail.com>
2025-11-19 22:12:20 +0100

    Testing transparent, it doesnt detect mouse

--- Assets/Scripts/UI/TransparentGame.cs
+++ Assets/Scripts/UI/TransparentGame.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class TransparentGame : MonoBehaviour
 {
@@ -10,12 +11,15 @@ public class TransparentGame : MonoBehaviour
     [Tooltip("Enable click-through for the window")]
     private bool m_enableClickThrough = true;
     private bool m_isClickThroughEnabled = false;
-    
+
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
     const int GWL_STYLE   = -16;
+    const int GWL_EXSTYLE = -20;
     const int WS_BORDER   = 0x00800000;
     const int WS_DLGFRAME = 0x00400000;
     const int WS_CAPTION  = WS_BORDER | WS_DLGFRAME;
+    const uint WS_EX_LAYERED = 0x00080000;
+    const uint WS_EX_TRANSPARENT = 0x00000020;
 
     [StructLayout(LayoutKind.Sequential)]
     struct MARGINS
@@ -39,11 +43,13 @@ public class TransparentGame : MonoBehaviour
     [DllImport("dwmapi.dll")]
     static extern int DwmExtendFrameIntoClientArea
         (IntPtr hWnd, ref MARGINS pMargins);
+
+    private IntPtr hwnd;
 #endif
     void Start()
     {
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
-        IntPtr hwnd = GetActiveWindow();
+        hwnd = GetActiveWindow();
 
         int style = GetWindowLong(hwnd, GWL_STYLE);
         style &= ~WS_CAPTION;
@@ -57,6 +63,11 @@ public class TransparentGame : MonoBehaviour
             cyBottomHeight = 0
         };
         DwmExtendFrameIntoClientArea(hwnd, ref margins);
+
+        // Set WS_EX_LAYERED to enable transparency effects
+        int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+        exStyle |= (int)WS_EX_LAYERED;
+        SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
 #else
         Debug.LogWarning($"[{nameof(TransparentGame)}] Transparent Game only works in Windows builds");
 
@@ -65,54 +76,78 @@ public class TransparentGame : MonoBehaviour
 
     private void Update()
     {
-#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
-            if (m_enableClickThrough)
-            {
-                UpdateClickThrough();
-            }
-#endif
+        if (m_enableClickThrough)
+        {
+            UpdateClickThrough();
+        }
     }
     
     private void UpdateClickThrough()
     {
-#if !UNITY_EDITOR && (UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN)
-            bool shouldBlockClick = IsMouseOverUI();
-
-            if (shouldBlockClick && m_isClickThroughEnabled)
-            {
-                DisableClickThrough();
-            }
-            else if (!shouldBlockClick && !m_isClickThroughEnabled)
-            {
-                EnableClickThrough();
-            }
-#endif
-    }
-    
-    private bool IsMouseOverUI()
-    {
-        if (EventSystem.current == null)
+        bool shouldBlockClick = ShouldBlockClick();
+        
+        if (shouldBlockClick && m_isClickThroughEnabled)
         {
-            return false;
+            DisableClickThrough();
+        }
+        else if (!shouldBlockClick && !m_isClickThroughEnabled)
+        {
+            EnableClickThrough();
+        }
+    }
+
+    private bool ShouldBlockClick()
+    {
+        // Check if mouse is over UI element
+        if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+        {
+            return true;
         }
 
-        return EventSystem.current.IsPointerOverGameObject();
+        // Check if mouse is over a 3D object with collider
+        if (Camera.main != null && Mouse.current != null)
+        {
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                return true;
+            }
+
+            // Check if mouse is over a 2D object with collider
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+            RaycastHit2D hit2D = Physics2D.Raycast(worldPoint, Vector2.zero);
+            if (hit2D.collider != null)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
     
     private void EnableClickThrough()
     {
-#if !UNITY_EDITOR && (UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN)
-            
-            m_isClickThroughEnabled = true;
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+            int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            exStyle |= (int)WS_EX_TRANSPARENT;
+            SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
 #endif
+        
+        m_isClickThroughEnabled = true;
+        Debug.Log("Enable click-through for the window");
     }
 
     private void DisableClickThrough()
     {
-#if !UNITY_EDITOR && (UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN)
-            
-            m_isClickThroughEnabled = false;
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+            int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            exStyle &= ~(int)WS_EX_TRANSPARENT;
+            SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
 #endif
+        
+        m_isClickThroughEnabled = false;
+        Debug.Log("Disable click-through for the window");
     }
 
     public void SetClickThrough(bool enabled)
