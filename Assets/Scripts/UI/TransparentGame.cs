ed7f08bd912b932ec8548e20a3489e7443ee3f9f
MARCPC\marcg <marc.g.e@hotmail.com>
2025-11-20 18:27:05 +0100

    Made game be always on top

--- Assets/Scripts/UI/TransparentGame.cs
+++ Assets/Scripts/UI/TransparentGame.cs
@@ -26,6 +26,12 @@ public class TransparentGame : MonoBehaviour
     const uint WS_EX_LAYERED = 0x00080000;
     const uint WS_EX_TRANSPARENT = 0x00000020;
 
+    // SetWindowPos constants for always on top
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    const uint SWP_NOSIZE = 0x0001;
+    const uint SWP_NOMOVE = 0x0002;
+    const uint SWP_SHOWWINDOW = 0x0040;
+
     [StructLayout(LayoutKind.Sequential)]
     struct MARGINS
     {
@@ -61,6 +67,9 @@ public class TransparentGame : MonoBehaviour
     [DllImport("dwmapi.dll")]
     static extern int DwmExtendFrameIntoClientArea
         (IntPtr hWnd, ref MARGINS pMargins);
+
+    [DllImport("user32.dll")]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
     private IntPtr hwnd;
 #endif
@@ -105,6 +114,10 @@ public class TransparentGame : MonoBehaviour
         int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
         exStyle |= (int)WS_EX_LAYERED;
         SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
+
+        // Make window always on top
+        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+        DebugBase.Log($"[{nameof(TransparentGame)}] Window set to always on top");
 #else
         DebugBase.LogWarning($"[{nameof(TransparentGame)}] Transparent Game only works in Windows builds", DebugCategory.Transparency);
 
