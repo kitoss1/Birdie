f4a499d427ef36dc44b01a67aeb85a867a815899
MARCPC\marcg <marc.g.e@hotmail.com>
2025-11-23 22:25:04 +0100

    Added threshold aplha clickeable for the birds

--- Assets/Scripts/Birds/BirdClickForwarder.cs
+++ Assets/Scripts/Birds/BirdClickForwarder.cs
@@ -1,6 +1,7 @@
 using Birdie.Debug;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Birdie.Birds
 {
@@ -15,8 +16,29 @@ namespace Birdie.Birds
         [Tooltip("Reference to the Bird component")]
         private Bird m_bird;
 
+        [SerializeField]
+        [Tooltip("Alpha threshold for hit testing (0-1). Only pixels above this alpha value will be clickable.")]
+        [Range(0f, 1f)]
+        private float m_alphaHitTestThreshold = 0.5f;
+
+        private Image m_image;
+
+        private void Awake()
+        {
+            m_image = GetComponent<Image>();
+            if (m_image != null)
+            {
+                m_image.alphaHitTestMinimumThreshold = m_alphaHitTestThreshold;
+            }
+            else
+            {
+                DebugBase.LogWarning($"[{nameof(BirdClickForwarder)}] No Image component found! Alpha hit testing will not work.", DebugCategory.Birds);
+            }
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
+            DebugBase.LogWarning($"[{nameof(BirdClickForwarder)}] Bird clicked", DebugCategory.Birds);
             if (m_bird == null)
             {
                 DebugBase.LogWarning($"[{nameof(BirdClickForwarder)}] Bird reference is not assigned!", DebugCategory.Birds);
